8a97661a0d28c8a179e7fd149516ad8be5f7003a
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-05-11 13:19:09 +0530

    add trans to already deplpyed proj

--- fincs3.py
+++ fincs3.py
@@ -1,14 +1,17 @@
 import boto3
+translate = boto3.client("translate")
 s3 = boto3.client("s3")
 
 def handler(request):
     try:
-        data = s3.list_objects(
-            Bucket="cloud9-ktest",
-            MaxKeys=10
+        data = translate.translate_text(
+            SourceLanguageCode="auto",
+            TargetLanguageCode="en",
+            Text="hola"
         )
     except BaseException as e:
         print(e)
         raise(e)
+    
     
     return "Successfully executed"
