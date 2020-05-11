980c8d3f9173bac3e4600c576c3fd6584384fd57
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-05-11 13:21:28 +0530

    add s3 agin removing trans from failed pro

--- fincs3.py
+++ fincs3.py
@@ -3,11 +3,11 @@ translate = boto3.client("translate")
 s3 = boto3.client("s3")
 
 def handler(request):
+   
     try:
-        data = translate.translate_text(
-            SourceLanguageCode="auto",
-            TargetLanguageCode="en",
-            Text="hola"
+        data = s3.list_objects(
+            Bucket="cloud9-ktest",
+            MaxKeys=10
         )
     except BaseException as e:
         print(e)
